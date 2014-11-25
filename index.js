a21dbf8be240d123ab326e3e27a3338e79bdeb0b
nmazour96 <nmazour@nebrwesleyan.edu>
2014-11-25 10:52:41 -0500

    changed links to paragraphs

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 "use strict"; // This line chooses a JavaScript dialect, one that helps both jsLint (used in OrionHub) and browsers catch errors.
 /*jslint browser: true*/ // This line tells jsLint that the code will run in a browser.
 
+function main_blog() {
+	
+}
