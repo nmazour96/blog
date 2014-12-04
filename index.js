e2f4052268474effedb245b63caa78dd8deb296e
nmazour96 <nmazour@nebrwesleyan.edu>
2014-12-04 13:45:44 -0500

    got rid of counter attempt

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 /*jslint browser: true*/ // This line tells jsLint that the code will run in a browser.
 
 var main = document.getElementById('main');
-var mainCounter = document.getELementById('mainCounter');
-main.addEventListener('click', main_clicks);
 var wonderOne = document.getElementById('wonderOne');
 var wonderTwo = document.getElementById('wonderTwo');
 var wonderThree = document.getElementById('wonderThree');
@@ -12,9 +10,3 @@ var wonderFive = document.getElementById('wonderFive');
 var wonderSix = document.getElementById('wonderSix');
 var wonderSeven = document.getElementById('wonderSeven');
 
-var count = 1;
-
-function main_clicks() {
-	mainCounter.innerHTML = count;
-	count = count + 1;
-}
