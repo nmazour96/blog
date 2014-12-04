6418708e99cbc81645bd597dc81242e159a674c1
nmazour96 <nmazour@nebrwesleyan.edu>
2014-12-04 14:43:44 -0500

    changed function names and disabled ratings after one rate

--- index.js
+++ index.js
@@ -16,19 +16,19 @@ starOneOneC.addEventListener('click', one_starOneC);
 var starOneOneFC = document.getElementById('starOneOneFC');
 starOneOneFC.style.display = 'none';
 var starTwoOneC = document.getElementById('starTwoOneC');
-starTwoOneC.addEventListener('click', one_starTwoC);
+starTwoOneC.addEventListener('click', two_starsOneC);
 var starTwoOneFC = document.getElementById('starTwoOneFC');
 starTwoOneFC.style.display = 'none';
 var starThreeOneC = document.getElementById('starThreeOneC');
-starThreeOneC.addEventListener('click', one_starThreeC);
+starThreeOneC.addEventListener('click', three_starsOneC);
 var starThreeOneFC = document.getElementById('starThreeOneFC');
 starThreeOneFC.style.display = 'none';
 var starFourOneC = document.getElementById('starFourOneC');
-starFourOneC.addEventListener('click', one_starFourC);
+starFourOneC.addEventListener('click', four_starsOneC);
 var starFourOneFC = document.getElementById('starFourOneFC');
 starFourOneFC.style.display = 'none';
 var starFiveOneC = document.getElementById('starFiveOneC');
-starFiveOneC.addEventListener('click', one_starFiveC);
+starFiveOneC.addEventListener('click', five_starsOneC);
 var starFiveOneFC = document.getElementById('starFiveOneFC');
 starFiveOneFC.style.display = 'none';
 
@@ -38,24 +38,69 @@ function one_starOneC() {
 	starOneOneC.style.display = 'none';
 	starOneOneFC.style.display = 'inline-block';
 	ratingOneC.innerHTML = '1 STAR';
+	starOneOneC.addEventListener('click', one_starOneC);
+	starTwoOneC.addEventListener('click', two_starsOneC);
+	starThreeOneC.addEventListener('click', three_starsOneC);
+	starFourOneC.addEventListener('click', four_starsOneC);
+	starFiveOneC.addEventListener('click', five_starsOneC);
 }
-function one_starTwoC() {
+function two_starsOneC() {
 	starTwoOneC.style.display = 'none';
+	starOneOneC.style.display = 'none';
 	starTwoOneFC.style.display = 'inline-block';
+	starOneOneFC.style.display = 'inline-block';
 	ratingOneC.innerHTML = '2 STARS';
+	starOneOneC.addEventListener('click', one_starOneC);
+	starTwoOneC.addEventListener('click', two_starsOneC);
+	starThreeOneC.addEventListener('click', three_starsOneC);
+	starFourOneC.addEventListener('click', four_starsOneC);
+	starFiveOneC.addEventListener('click', five_starsOneC);
 }
-function one_starThreeC() {
+function three_starsOneC() {
 	starThreeOneC.style.display = 'none';
+	starTwoOneC.style.display = 'none';
+	starOneOneC.style.display = 'none';
 	starThreeOneFC.style.display = 'inline-block';
+	starTwoOneFC.style.display = 'inline-block';
+	starOneOneFC.style.display = 'inline-block';
 	ratingOneC.innerHTML = '3 STARS';
+	starOneOneC.addEventListener('click', one_starOneC);
+	starTwoOneC.addEventListener('click', two_starsOneC);
+	starThreeOneC.addEventListener('click', three_starsOneC);
+	starFourOneC.addEventListener('click', four_starsOneC);
+	starFiveOneC.addEventListener('click', five_starsOneC);
 }
-function one_starFourC() {
+function four_starsOneC() {
 	starFourOneC.style.display = 'none';
+	starThreeOneC.style.display = 'none';
+	starTwoOneC.style.display = 'none';
+	starOneOneC.style.display = 'none';
 	starFourOneFC.style.display = 'inline-block';
+	starThreeOneFC.style.display = 'inline-block';
+	starTwoOneFC.style.display = 'inline-block';
+	starOneOneFC.style.display = 'inline-block';
 	ratingOneC.innerHTML = '4 STARS';
+	starOneOneC.addEventListener('click', one_starOneC);
+	starTwoOneC.addEventListener('click', two_starsOneC);
+	starThreeOneC.addEventListener('click', three_starsOneC);
+	starFourOneC.addEventListener('click', four_starsOneC);
+	starFiveOneC.addEventListener('click', five_starsOneC);
 }
-function one_starFiveC() {
+function five_starsOneC() {
 	starFiveOneC.style.display = 'none';
+	starFourOneC.style.display = 'none';
+	starThreeOneC.style.display = 'none';
+	starTwoOneC.style.display = 'none';
+	starOneOneC.style.display = 'none';
 	starFiveOneFC.style.display = 'inline-block';
+	starFourOneFC.style.display = 'inline-block';
+	starThreeOneFC.style.display = 'inline-block';
+	starTwoOneFC.style.display = 'inline-block';
+	starOneOneFC.style.display = 'inline-block';
 	ratingOneC.innerHTML = '5 STARS';
+	starOneOneC.addEventListener('click', one_starOneC);
+	starTwoOneC.addEventListener('click', two_starsOneC);
+	starThreeOneC.addEventListener('click', three_starsOneC);
+	starFourOneC.addEventListener('click', four_starsOneC);
+	starFiveOneC.addEventListener('click', five_starsOneC);
 }
