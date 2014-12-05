667e5ed16e7e29649919c6629c4dc43227d9b093
nmazour96 <nmazour@nebrwesleyan.edu>
2014-12-04 21:17:03 -0500

    added like and dislike to first wonder

--- index.js
+++ index.js
@@ -10,97 +10,76 @@ var wonderFive = document.getElementById('wonderFive');
 var wonderSix = document.getElementById('wonderSix');
 var wonderSeven = document.getElementById('wonderSeven');
 
-/*Star variable names and ids are star-starNumber-postNumber-wonderLetter*/
-var starOneOneC = document.getElementById('starOneOneC');
-starOneOneC.addEventListener('click', one_starOneC);
-var starOneOneFC = document.getElementById('starOneOneFC');
-starOneOneFC.style.display = 'none';
-var starTwoOneC = document.getElementById('starTwoOneC');
-starTwoOneC.addEventListener('click', two_starsOneC);
-var starTwoOneFC = document.getElementById('starTwoOneFC');
-starTwoOneFC.style.display = 'none';
-var starThreeOneC = document.getElementById('starThreeOneC');
-starThreeOneC.addEventListener('click', three_starsOneC);
-var starThreeOneFC = document.getElementById('starThreeOneFC');
-starThreeOneFC.style.display = 'none';
-var starFourOneC = document.getElementById('starFourOneC');
-starFourOneC.addEventListener('click', four_starsOneC);
-var starFourOneFC = document.getElementById('starFourOneFC');
-starFourOneFC.style.display = 'none';
-var starFiveOneC = document.getElementById('starFiveOneC');
-starFiveOneC.addEventListener('click', five_starsOneC);
-var starFiveOneFC = document.getElementById('starFiveOneFC');
-starFiveOneFC.style.display = 'none';
+var upOneC = document.getElementById('upOneC');
+upOneC.addEventListener('click', UpOneC);
+var upTwoC = document.getElementById('upTwoC');
+upTwoC.addEventListener('click', UpTwoC);
+var upThreeC = document.getElementById('upThreeC');
+upThreeC.addEventListener('click', UpThreeC);
+var upFourC = document.getElementById('upFourC');
+upFourC.addEventListener('click', UpFourC);
+var upFiveC = document.getElementById('upFiveC');
+upFiveC.addEventListener('click', UpFiveC);
 
-var ratingOneC = document.getElementById('ratingOneC');
+var downOneC = document.getElementById('downOneC');
+downOneC.addEventListener('click', DownOneC);
+var downTwoC = document.getElementById('downTwoC');
+downTwoC.addEventListener('click', DownTwoC);
+var downThreeC = document.getElementById('downThreeC');
+downThreeC.addEventListener('click', DownThreeC);
+var downFourC = document.getElementById('downFourC');
+downFourC.addEventListener('click', DownFourC);
+var downFiveC = document.getElementById('downFiveC');
+downFiveC.addEventListener('click', DownFiveC);
 
-function one_starOneC() {
-	starOneOneC.style.display = 'none';
-	starOneOneFC.style.display = 'inline-block';
-	ratingOneC.innerHTML = '1 STAR';
-	starOneOneC.removeEventListener('click', one_starOneC);
-	starTwoOneC.removeEventListener('click', two_starsOneC);
-	starThreeOneC.removeEventListener('click', three_starsOneC);
-	starFourOneC.removeEventListener('click', four_starsOneC);
-	starFiveOneC.removeEventListener('click', five_starsOneC);
+function UpOneC() {
+	upOneC.style.color = '#071';
+	upOneC.removeEventListener('click', UpOneC);
+	downOneC.removeEventListener('click', DownOneC);
 }
-function two_starsOneC() {
-	starTwoOneC.style.display = 'none';
-	starOneOneC.style.display = 'none';
-	starTwoOneFC.style.display = 'inline-block';
-	starOneOneFC.style.display = 'inline-block';
-	ratingOneC.innerHTML = '2 STARS';
-	starOneOneC.removeEventListener('click', one_starOneC);
-	starTwoOneC.removeEventListener('click', two_starsOneC);
-	starThreeOneC.removeEventListener('click', three_starsOneC);
-	starFourOneC.removeEventListener('click', four_starsOneC);
-	starFiveOneC.removeEventListener('click', five_starsOneC);
+function UpTwoC() {
+	upTwoC.style.color = '#071';
+	upTwoC.removeEventListener('click', UpTwoC);
+	downTwoC.removeEventListener('click', DownTwoC);
 }
-function three_starsOneC() {
-	starThreeOneC.style.display = 'none';
-	starTwoOneC.style.display = 'none';
-	starOneOneC.style.display = 'none';
-	starThreeOneFC.style.display = 'inline-block';
-	starTwoOneFC.style.display = 'inline-block';
-	starOneOneFC.style.display = 'inline-block';
-	ratingOneC.innerHTML = '3 STARS';
-	starOneOneC.removeEventListener('click', one_starOneC);
-	starTwoOneC.removeEventListener('click', two_starsOneC);
-	starThreeOneC.removeEventListener('click', three_starsOneC);
-	starFourOneC.removeEventListener('click', four_starsOneC);
-	starFiveOneC.removeEventListener('click', five_starsOneC);
+function UpThreeC() {
+	upThreeC.style.color = '#071';
+	upThreeC.removeEventListener('click', UpThreeC);
+	downThreeC.removeEventListener('click', DownThreeC);
 }
-function four_starsOneC() {
-	starFourOneC.style.display = 'none';
-	starThreeOneC.style.display = 'none';
-	starTwoOneC.style.display = 'none';
-	starOneOneC.style.display = 'none';
-	starFourOneFC.style.display = 'inline-block';
-	starThreeOneFC.style.display = 'inline-block';
-	starTwoOneFC.style.display = 'inline-block';
-	starOneOneFC.style.display = 'inline-block';
-	ratingOneC.innerHTML = '4 STARS';
-	starOneOneC.removeEventListener('click', one_starOneC);
-	starTwoOneC.removeEventListener('click', two_starsOneC);
-	starThreeOneC.removeEventListener('click', three_starsOneC);
-	starFourOneC.removeEventListener('click', four_starsOneC);
-	starFiveOneC.removeEventListener('click', five_starsOneC);
+function UpFourC() {
+	upFourC.style.color = '#071';
+	upFourC.removeEventListener('click', UpFourC);
+	downFourC.removeEventListener('click', DownFourC);
 }
-function five_starsOneC() {
-	starFiveOneC.style.display = 'none';
-	starFourOneC.style.display = 'none';
-	starThreeOneC.style.display = 'none';
-	starTwoOneC.style.display = 'none';
-	starOneOneC.style.display = 'none';
-	starFiveOneFC.style.display = 'inline-block';
-	starFourOneFC.style.display = 'inline-block';
-	starThreeOneFC.style.display = 'inline-block';
-	starTwoOneFC.style.display = 'inline-block';
-	starOneOneFC.style.display = 'inline-block';
-	ratingOneC.innerHTML = '5 STARS';
-	starOneOneC.removeEventListener('click', one_starOneC);
-	starTwoOneC.removeEventListener('click', two_starsOneC);
-	starThreeOneC.removeEventListener('click', three_starsOneC);
-	starFourOneC.removeEventListener('click', four_starsOneC);
-	starFiveOneC.removeEventListener('click', five_starsOneC);
+function UpFiveC() {
+	upFiveC.style.color = '#071';
+	upFiveC.removeEventListener('click', UpFiveC);
+	downFiveC.removeEventListener('click', DownFiveC);
+}
+
+function DownOneC() {
+	downOneC.style.color = '#F00';
+	upOneC.removeEventListener('click', UpOneC);
+	downOneC.removeEventListener('click', DownOneC);
+}
+function DownTwoC() {
+	downTwoC.style.color = '#F00';
+	upTwoC.removeEventListener('click', UpTwoC);
+	downTwoC.removeEventListener('click', DownTwoC);
+}
+function DownThreeC() {
+	downThreeC.style.color = '#F00';
+	upThreeC.removeEventListener('click', UpThreeC);
+	downThreeC.removeEventListener('click', DownThreeC);
+}
+function DownFourC() {
+	downFourC.style.color = '#F00';
+	upFourC.removeEventListener('click', UpFourC);
+	downFourC.removeEventListener('click', DownFourC);
+}
+function DownFiveC() {
+	downFiveC.style.color = '#F00';
+	upFiveC.removeEventListener('click', UpFiveC);
+	downFiveC.removeEventListener('click', DownFiveC);
 }
